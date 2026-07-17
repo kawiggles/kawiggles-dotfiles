b51cfd48144b7f96a07bda4579e0b5d49a3c1c41
Kawiggles <kawikadembroski@gmail.com>
2026-07-17 11:36:56 -0600

    feat: no longer reliant on .vimrc

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,12 +1,12 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git", "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -14,10 +14,16 @@ require('lazy').setup(require('plugins'))
 require('lsp')
 require('keymaps')
 
-vim.cmd('source ~/.vimrc')
 vim.cmd.colorscheme("oceanic_material")
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.colorcolumn = "100"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+
 vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
