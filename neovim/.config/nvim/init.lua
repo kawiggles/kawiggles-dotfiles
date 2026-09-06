fc950945a0a52a98645df545a18297745215f1fd
Kawika Dembroski <kawiggles@great-eastern.kawiggles.com>
2026-09-06 02:23:13 -0600

    feat: shell configuration

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		'git', 'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable'
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup(require('plugins'))
+require('keymaps')
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.colorcoloum = '80'
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'markdown', 'gitcommit' },
+	callback = function ()
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = 'en_us'
+	end,
+})
--- shell/.zshrc
+++ shell/.zshrc
@@ -5,18 +5,23 @@ HISTFILE=~/.histfile
 HISTSIZE=1000
 SAVEHIST=1000
 
-export PROMPT="F{red}%n@%m%f %~%# "
 export EDITOR="/usr/bin/vim"
 
 alias ..="cd .."
 alias ls="ls --color=auto --group-directories-first"
 alias ll="ls -lah --color=auto --group-directories-first"
 
+nvif() {
+	local file
+	file=$(fzf --query="$1") && [ -n "$file" ] && nvim "$file"
+}
+
 cd ~
 fastfetch
 eval "$(fzf --zsh)"
 eval "$(zoxide init zsh)"
 eval "$(atuin init zsh)"
+eval "$(starship init zsh)"
 
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
